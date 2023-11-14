4302573b972ce6433e2cada8babe55bef9775811
gudokk <masagudkova0@gmail.com>
2023-11-14 19:17:37 +0700

    предмет интересный и лабы замечательные

--- ПЗ 3 и 4 Test/TrigFunctionTest.cs
+++ ПЗ 3 и 4 Test/TrigFunctionTest.cs
@@ -47,5 +47,113 @@ namespace ПЗ_3_и_4_Test
             Assert.AreEqual(expr.IsConstant, true);
             Assert.AreEqual(expr.ToString(), "Cos(7)");
         }
+        [TestMethod]
+        public void SinTestPos()
+        {
+            var x = new Variable("x");
+            var expr = new Sin(x);
+            Assert.AreEqual(expr.Compute(new Dictionary<string, double>{["x"] = 1}),Math.Sin(1), 0.0001);
+        }
+
+        [TestMethod]
+        public void SinTestNeg()
+        {
+            var x = new Variable("x");
+            var expr = new Sin(x);
+            Assert.AreEqual(expr.Compute(new Dictionary<string, double>{["x"]=-1}),Math.Sin(-1),0.0001 );
+        }
+
+        [TestMethod]
+        public void SinVariable()
+        {
+            var a = new Variable("a");
+            var expr = new Sin(a);
+            Assert.AreEqual(expr.IsPolynom, false);
+            Assert.AreEqual(expr.IsConstant, false);
+            Assert.AreEqual(expr.ToString(), "Sin(a)");
+        }
+        
+        [TestMethod]
+        public void SinConstant()
+        {
+            var a = new Constant(7);
+            var expr = new Sin(a);
+            Assert.AreEqual(expr.IsPolynom, true);
+            Assert.AreEqual(expr.IsConstant, true);
+            Assert.AreEqual(expr.ToString(), "Sin(7)");
+        } 
+        
+        [TestMethod]
+        public void CotTestPos()
+        {
+            var x = new Variable("x");
+            var expr = new Cot(x);
+            Assert.AreEqual(expr.Compute(new Dictionary<string, double>{["x"] = 1}),Math.Cot(1), 0.0001);
+        }
+
+        [TestMethod]
+        public void CotTestNeg()
+        {
+            var x = new Variable("x");
+            var expr = new Cot(x);
+            Assert.AreEqual(expr.Compute(new Dictionary<string, double>{["x"]=-1}),Math.Cot(-1),0.0001 );
+        }
+
+        [TestMethod]
+        public void CotVariable()
+        {
+            var a = new Variable("a");
+            var expr = new Cot(a);
+            Assert.AreEqual(expr.IsPolynom, false);
+            Assert.AreEqual(expr.IsConstant, false);
+            Assert.AreEqual(expr.ToString(), "Cot(a)");
+        }
+        
+        [TestMethod]
+        public void CotConstant()
+        {
+            var a = new Constant(7);
+            var expr = new Cot(a);
+            Assert.AreEqual(expr.IsPolynom, true);
+            Assert.AreEqual(expr.IsConstant, true);
+            Assert.AreEqual(expr.ToString(), "Cot(7)");
+        } 
+        
+        //тангенс
+        [TestMethod]
+        public void TanTestPos()
+        {
+            var x = new Variable("x");
+            var expr = new Tan(x);
+            Assert.AreEqual(expr.Compute(new Dictionary<string, double>{["x"] = 1}),Math.Tan(1), 0.0001);
+        }
+
+        [TestMethod]
+        public void TanTestNeg()
+        {
+            var x = new Variable("x");
+            var expr = new Tan(x);
+            Assert.AreEqual(expr.Compute(new Dictionary<string, double>{["x"]=-1}),Math.Tan(-1),0.0001 );
+        }
+
+        [TestMethod]
+        public void TanVariable()
+        {
+            var a = new Variable("a");
+            var expr = new Tan(a);
+            Assert.AreEqual(expr.IsPolynom, false);
+            Assert.AreEqual(expr.IsConstant, false);
+            Assert.AreEqual(expr.ToString(), "Tan(a)");
+        }
+        
+        [TestMethod]
+        public void TanConstant()
+        {
+            var a = new Constant(7);
+            var expr = new Tan(a);
+            Assert.AreEqual(expr.IsPolynom, true);
+            Assert.AreEqual(expr.IsConstant, true);
+            Assert.AreEqual(expr.ToString(), "Tan(7)");
+        } 
    }
 }
